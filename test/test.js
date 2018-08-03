34771a280f96e3afc7efd0f85331501067534c02
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2018-08-03 11:39:00 -0500

    Finished up tests and bug fixes for v1.

--- test/test.js
+++ test/test.js
@@ -4,6 +4,26 @@ const AD = require('../index').ActiveDirectory;
 //const testConfig = { url: '', suffix: '' }
 //const  = new index.ActiveDirectory(testConfig);
 
+const testEntry = {
+    object: {
+        memberOf: [
+            'CN=Group1,OU=Test,DC=domain,DC=com',
+            'CN=Group2,OU=Test,OU=Test2,DC=domain,DC=com',
+        ],
+        mail: 'test@domain.com',
+        telephoneNumber: '123-123-1234',
+        name: 'Test User'
+    },
+    attributes: [
+        {
+            type: 'objectGUID',
+            buffers: [
+                Buffer.from('10E7D4174D627849900B8549CB753699', 'hex')
+            ]
+        }
+    ]
+}
+
 describe('Static Functions', () => {
     describe('#resolveBindError()', () => {
         it('Should return unkown if random text is sent', () => {
@@ -28,23 +48,40 @@ describe('Static Functions', () => {
         });
     });
 
-    // TODO: Write unit tests
-    // describe('#resolveGUID()', () => {
-        
-    // });
+    describe('#resolveGroups()', () => {
+        it('User only has groups in 1 OU', () => {
+            const tmpEntry = {...testEntry, object: { memberOf: [...testEntry.object.memberOf] }};
+            tmpEntry.object.memberOf = 'CN=Group1,CN=Group2,DC=domain,DC=com';
+            assert.deepStrictEqual(
+                AD.resolveGroups(tmpEntry),
+                ['Group1', 'Group2']
+            );
+        });
+
+        it('Has groups in multiple OUs', () => {
+            assert.deepStrictEqual(
+                AD.resolveGroups(testEntry),
+                ['Group1', 'Group2']
+            );
+        });
+
+        it('If invalid argument it should return empty array', () => {
+            const tmpEntry = {...testEntry, object: { memberOf: [...testEntry.object.memberOf] }};
+            tmpEntry.object.memberOf = undefined;
+            assert.deepStrictEqual(
+                AD.resolveGroups(tmpEntry),
+                []
+            );
+        });
+
+        it('If invalid entry it should throw an error', () => {
+            let tmpEntry = {...testEntry};
+            tmpEntry = undefined;
+            assert.throws(() => AD.resolveGroups(tmpEntry), Error);
+        });
+    });
 
     describe('#resolveGUID()', () => {
-        const testEntry = {
-            attributes: [
-                {
-                    type: 'objectGUID',
-                    buffers: [
-                        Buffer.from('10E7D4174D627849900B8549CB753699', 'hex')
-                    ]
-                }
-            ]
-        }
-
         it('Convert hex buffer into formatted guid string', () => {
             assert.equal(
                 AD.resolveGUID(testEntry),
@@ -52,21 +89,29 @@ describe('Static Functions', () => {
             );
         });
 
-        it('If entry is not it should throw error', () => {
+        it('If entry is not valid it should throw error', () => {
             const tmpEntry = undefined;
             assert.throws(() => AD.resolveGUID(tmpEntry), Error);
         });
-
-        it('If no attributes it should throw error', () => {
-            const tmpEntry = {...testEntry};
-            delete tmpEntry.attributes;
-            assert.throws(() => AD.resolveGUID(tmpEntry), Error);
-        });
-
     });
 
-    // TODO: Write unit tests
-    // describe('#createUserObj()', () => {
+    describe('#createUserObj()', () => {
+        it('Should return valid user object', () => {
+            const userObj = {
+                groups: ['Group1', 'Group2'],
+                mail: 'test@domain.com',
+                phone: '123-123-1234',
+                name: 'Test User',
+                guid: '17d4e710-624d-4978-900b-8549cb753699'
+            }
+            
+            assert.deepStrictEqual(
+                AD.createUserObj(testEntry), userObj);
+        });
         
-    // });
+        it('If invalid entry it should throw an error', () => {
+            const tmpEntry = undefined;
+            assert.throws(() => AD.createUserObj(tmpEntry), Error);
+        });
+    });
   });
